c798e0ab34137e4c8013491af7836a01c989a9cc
Jonathan Bartlett <jonathan@bartlettpublishing.com>
2022-05-23 23:54:41 -0500

    Working on initial API ideas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    def current_uid 
+        "none"
+    end
+
+    def has_permission?(name)
+        return true
+    end
 end
---
+++ db/migrate/20220524033456_generate_initial_tables.rb
@@ -0,0 +1,49 @@
+class GenerateInitialTables < ActiveRecord::Migration[7.0]
+  def change
+    create_table :channels do |t|
+      t.string :name
+      t.string :owner_uid
+
+      t.timestamps
+    end
+    add_index :channels, :name, :unique => true
+    add_index :channels, :owner_uid
+
+    create_table :messages do |t|
+      t.references :channel
+      t.string :message_origination_reference
+      t.string :message_reference
+      t.string :publisher_uid
+      t.blob :message
+
+      t.timestamps
+    end
+    add_index :messages, [:channel_id, :id]
+    add_index :messages, :message_reference, :unique => true
+    add_index :messages, [:channel_id, :message_origination_reference], :unique => true
+
+    create_table :message_metadata do |t|
+      t.references :message
+      t.string :key
+      t.string :value
+    end
+
+    create_table :subscriptions do |t|
+      t.references :channel
+      t.references :default_message_cursor
+      t.string :name
+      t.string :subscriber_uid
+
+      t.timestamps
+    end
+    add_index :subscriptions, [:channel_id, :name], :unique => true
+
+    create_table :message_cursors do |t|
+      t.string :originator_uid
+      t.references :channel
+      t.references :last_message
+
+      t.timestamps
+    end
+  end
+end

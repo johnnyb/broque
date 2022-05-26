e16afe4b1edd44435a83026bac3b80d8c5cbb3c8
Jonathan Bartlett <jbartlett@mcelroy.com>
2022-05-26 08:42:07 -0500

    Refactored

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -12,4 +12,20 @@ class Channel < ApplicationRecord
 		return nil unless has_permission?(uid, nil, :channel_create)
 		return Channel.create!(:name => name, :owner_uid => uid)
 	end
+
+	def self.clean_expired_messages!
+		# Delete fully expired messages
+		Channel.where(:expire_messages => true).each do |ch|
+			# Kill all messages **and** active reads which are past the forced expiration time
+			if (ch.force_message_expiration_time || 0) > 0
+				ch.messages.where(:created_at => ..(Time.now - ch.force_message_expiration_time.seconds)).destroy_all
+			end
+
+			# Find the minimum ID of all readers
+			min_id = ch.message_cursors.minimum(:last_message_id)
+			# Delete everything below that number that doesn't have an active read associated (whether DLQ or not)
+			ch.messages.where(:id => ..min_id).and(ch.messages.where.not(ActiveReading.all.select(:message_id).arel.exists)).delete_all # Don't have to destroy because there are no associated records!
+			ch.messages.where(:id => ..min_id).and(where.not(:id => ActiveReading.all))
+		end
+	end
 end

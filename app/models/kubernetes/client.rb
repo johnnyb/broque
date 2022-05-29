227b6cd236c22067e6ff895597e0c9e0ae0950a6
Jonathan Bartlett <jonathan@bartlettpublishing.com>
2022-05-29 08:01:34 -0500

    Starting space->tab conversion.  I don't know why VS Code hates me and tried to make everything spaces

--- app/models/kubernetes/client.rb
+++ app/models/kubernetes/client.rb
@@ -1,50 +1,47 @@
 class Kubernetes::Client
-    include HTTParty
-    ssl_ca_file "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+	include HTTParty
+	ssl_ca_file "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 
-    attr_accessor :base_url
-    attr_accessor :namespace
-    attr_accessor :token
-    attr_accessor :token_path
+	attr_accessor :base_url
+	attr_accessor :namespace
+	attr_accessor :token
+	attr_accessor :token_path
 
-    def initialize(opts = {})
-        self.base_url = opts[:base_url] || "https://kubernetes.default.svc"
-        self.namespace = opts[:namespace] || begin
-            File.open("/var/run/secrets/kubernetes.io/serviceaccount/namespace").read
-        rescue
-            "default"
-        end
-        self.token_path = opts[:token_path] || "/var/run/secrets/kubernetes.io/serviceaccount/token"
-        self.token = opts[:token]
-    end
+	def initialize(opts = {})
+		self.base_url = opts[:base_url] || "https://kubernetes.default.svc"
+		self.namespace = opts[:namespace] || begin
+			File.open("/var/run/secrets/kubernetes.io/serviceaccount/namespace").read
+		rescue
+			"default"
+		end
+		self.token_path = opts[:token_path] || "/var/run/secrets/kubernetes.io/serviceaccount/token"
+		self.token = opts[:token]
+	end
 
-    def self.shared
-        @client ||= Kubernetes::Client.new
-        return @client
-    end
+	def self.shared
+		@client ||= Kubernetes::Client.new
+		return @client
+	end
 
-    def api_get(path)
-    end
+	def my_token
+		return token if token.present? # If a specific token is specified, use that
 
-    def my_token
-        return token if token.present? # If a specific token is specified, use that
+		# Periodically refresh token from the path in case they are cycling tokens
+		Rails.cache.fetch("auth/mytoken", :expires_in => 1.hour) do
+			File.open(token_path).read
+		end
+	end
 
-        # Periodically refresh token from the path in case they are cycling tokens
-        Rails.cache.fetch("auth/mytoken", :expires_in => 1.hour) do
-            File.open(token_path).read
-        end
-    end
+	def api_get(path)
+		self.class.get("#{base_url}#{path}", :headers => {
+			"Authorization" => "Bearer #{my_token}"
+		})
+	end
 
-    def api_get(path)
-        self.class.get("#{base_url}#{path}", :headers => {
-            "Authorization" => "Bearer #{my_token}"
-        })
-    end
-
-    def api_post(path, body)
-        self.class.post("#{base_url}#{path}", :headers => {
-            "Authorization" => "Bearer #{my_token}",
-            "Content-Type" => "application/json"
-        })
-    end 
+	def api_post(path, body)
+		self.class.post("#{base_url}#{path}", :headers => {
+			"Authorization" => "Bearer #{my_token}",
+			"Content-Type" => "application/json"
+		})
+	end 
 end

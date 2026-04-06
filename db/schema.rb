3c6a32ffed3f03cbd050bb638b6f0904bd556a3d
Jonathan Bartlett <jonathan@bartlettpublishing.com>
2026-04-06 06:55:59 -0500

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_30_150114) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_06_113109) do
   create_table "active_readings", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.boolean "died", default: false
+    t.datetime "expires_at", precision: nil
+    t.integer "max_reads"
     t.integer "message_cursor_id"
     t.integer "message_id"
-    t.datetime "expires_at", precision: nil
-    t.boolean "died", default: false
     t.integer "read_count", default: 0
-    t.integer "max_reads"
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["message_cursor_id", "expires_at"], name: "index_active_readings_on_message_cursor_id_and_expires_at"
     t.index ["message_cursor_id", "message_id"], name: "index_active_readings_on_message_cursor_id_and_message_id", unique: true
@@ -27,37 +27,37 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_30_150114) do
   end
 
   create_table "channels", force: :cascade do |t|
-    t.string "name"
-    t.string "owner_uid"
-    t.boolean "expire_messages", default: false
-    t.integer "force_message_expiration_time"
+    t.boolean "authentication_required", default: false
+    t.datetime "created_at", null: false
     t.integer "default_max_reads"
     t.integer "default_read_timeout"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean "authentication_required", default: false
+    t.boolean "expire_messages", default: false
+    t.integer "force_message_expiration_time"
+    t.string "name"
+    t.string "owner_uid"
     t.boolean "permission_required", default: false
+    t.datetime "updated_at", null: false
     t.index ["name"], name: "index_channels_on_name", unique: true
     t.index ["owner_uid"], name: "index_channels_on_owner_uid"
   end
 
   create_table "message_cursors", force: :cascade do |t|
-    t.string "originator_uid"
     t.integer "channel_id"
-    t.integer "last_message_id"
+    t.datetime "created_at", null: false
     t.integer "default_max_reads"
     t.integer "default_read_timeout"
-    t.datetime "created_at", null: false
+    t.integer "last_message_id"
+    t.string "originator_uid"
     t.datetime "updated_at", null: false
     t.index ["channel_id"], name: "index_message_cursors_on_channel_id"
     t.index ["last_message_id"], name: "index_message_cursors_on_last_message_id"
   end
 
   create_table "message_metadata", force: :cascade do |t|
-    t.integer "message_id"
-    t.string "key"
-    t.string "value"
     t.integer "channel_id"
+    t.string "key"
+    t.integer "message_id"
+    t.string "value"
     t.index ["channel_id", "key", "value"], name: "index_message_metadata_on_channel_id_and_key_and_value"
     t.index ["channel_id"], name: "index_message_metadata_on_channel_id"
     t.index ["message_id"], name: "index_message_metadata_on_message_id"
@@ -65,11 +65,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_30_150114) do
 
   create_table "messages", force: :cascade do |t|
     t.integer "channel_id"
+    t.datetime "created_at", null: false
+    t.text "message"
     t.string "message_origination_reference"
     t.string "message_reference"
     t.string "publisher_uid"
-    t.text "message"
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["channel_id", "id"], name: "index_messages_on_channel_id_and_id"
     t.index ["channel_id", "message_origination_reference"], name: "index_messages_on_channel_id_and_message_origination_reference", unique: true
@@ -78,24 +78,23 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_30_150114) do
   end
 
   create_table "permissions", force: :cascade do |t|
-    t.string "permission_on_type"
-    t.integer "permission_on_id"
-    t.string "uid"
     t.string "permission"
+    t.integer "permission_on_id"
+    t.string "permission_on_type"
+    t.string "uid"
     t.index ["permission_on_id"], name: "index_permissions_on_permission_on_id"
     t.index ["uid"], name: "index_permissions_on_uid"
   end
 
   create_table "subscriptions", force: :cascade do |t|
     t.integer "channel_id"
+    t.datetime "created_at", null: false
     t.integer "default_message_cursor_id"
     t.string "name"
     t.string "subscriber_uid"
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["channel_id", "name"], name: "index_subscriptions_on_channel_id_and_name", unique: true
     t.index ["channel_id"], name: "index_subscriptions_on_channel_id"
     t.index ["default_message_cursor_id"], name: "index_subscriptions_on_default_message_cursor_id"
   end
-
 end
